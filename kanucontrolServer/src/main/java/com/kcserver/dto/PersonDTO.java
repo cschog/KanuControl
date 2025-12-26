0caaddbbf2845393d9bdcdb6e70a650461e935cf
Chris Schog <c.schog@icloud.com>
2025-12-26 11:43:33 +0100

    Readme und Errorhandling
    - Readme Update
    - GlobalmExceptionHandling Update

--- kanucontrolServer/src/main/java/com/kcserver/dto/PersonDTO.java
+++ kanucontrolServer/src/main/java/com/kcserver/dto/PersonDTO.java
@@ -1,5 +1,8 @@
 package com.kcserver.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.List;
@@ -9,11 +12,23 @@ import java.util.List;
 @AllArgsConstructor
 public class PersonDTO {
     private Long id; // Matches 'id' in Person
+
+    @NotBlank(message = "Name must not be blank")
+    @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
     private String name; // Matches 'name' in Person
+
+    @NotBlank(message = "Vorname must not be blank")
+    @Size(min = 2, max = 100)
     private String vorname; // Matches 'vorname' in Person
     private String strasse; // Matches 'strasse' in Person
     private String plz; // Matches 'plz' in Person
     private String ort; // Matches 'ort' in Person
+
+
+    @Pattern(
+            regexp = "^[0-9+\\-/ ]*$",
+            message = "Telefon contains invalid characters"
+    )
     private String telefon; // Matches 'telefon' in Person
     private String bankName; // Matches 'bankName' in Person
     private String iban; // Matches 'iban' in Person
